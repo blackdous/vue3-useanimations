2bda10043a2da7613d863cece0a06819755ef8c7
jg.zhang <velen@likuan.com>
2023-06-08 10:15:04 +0800

    feat: 新增lottie icon

---
+++ src/lib/chevronLeft/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _chevronLeft = _interopRequireDefault(require("./chevronLeft.json"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _default = {
+  animationData: _chevronLeft["default"],
+  animationKey: 'chevronLeft'
+};
+exports["default"] = _default;
